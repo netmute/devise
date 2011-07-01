9ed2d482fbe2f46f7c30cbf53e6eb0a3cf445eb5
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-07-01 20:49:05 -0300

    Oops, corrected the test case

--- test/models/validatable_test.rb
+++ test/models/validatable_test.rb
@@ -28,7 +28,7 @@ class ValidatableTest < ActiveSupport::TestCase
     assert user.invalid?
     assert_not_equal 'is invalid', user.errors[:email].join
 
-    %w(invalid_email_format 123 $$$ \(\) ).each do |email|
+    %w(invalid_email_format 123 $$$ \(\) user@foo,com).each do |email|
       user.email = email
       assert user.invalid?, 'should be invalid with email ' << email
       assert_equal 'is invalid', user.errors[:email].join
@@ -39,7 +39,7 @@ class ValidatableTest < ActiveSupport::TestCase
   end
 
   test 'should accept valid emails' do
-    %w(a.b.c@example.com test_mail@gmail.com any@any.net email@test.br 123@mail.test user@foo,com).each do |email|
+    %w(a.b.c@example.com test_mail@gmail.com any@any.net email@test.br 123@mail.test ).each do |email|
       user = new_user(:email => email)
       assert user.valid?, 'should be valid with email ' << email
       assert_blank user.errors[:email]
